4e4d73255b13acd4f2f137de46156efe50d6ca52
Arthemis Hapa <arthemishapa@gmail.com>
2020-03-18 17:55:59 +0200

    Add TESTS

--- SSVV_Maven/src/test/java/Test/AppTest.java
+++ SSVV_Maven/src/test/java/Test/AppTest.java
@@ -1,20 +1,46 @@
 package Test;
 
-import static org.junit.Assert.assertTrue;
-
+import Domain.Nota;
+import Domain.Student;
+import Domain.Teme;
+import Repository.NoteRepo;
+import Repository.StudentRepo;
+import Repository.TemeRepo;
+import Service.ServiceNote;
+import Service.ServiceStudent;
+import Service.ServiceTeme;
+import Validator.StudentValidator;
+import Validator.TemeValidator;
+import Validator.NotaValidator;
+import Validator.Validator;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void addStudent() {
+        Validator<Student> studentValidator = new StudentValidator();
+
+        StudentRepo rep = new StudentRepo(studentValidator, "studenti.xml");
+        ServiceStudent srv = new ServiceStudent(rep);
+        assertEquals(new Student("123", "asda", 933, "asda@asda.casd", "asda"),
+                srv.add(new Student("123", "asda", 933, "asda@asda.casd", "asda")));
+    }
+
+    @Test
+    public void addStudentInvalid() {
+        Validator<Student> studentValidator = new StudentValidator();
+
+        StudentRepo rep = new StudentRepo(studentValidator, "studenti.xml");
+        ServiceStudent srv = new ServiceStudent(rep);
+        assertNotSame(new Student("123", "asda", 933, "asda@asda.casd", "asda"),
+                srv.add(new Student("124", "asda", 933, "asda@asda.casd", "asda")));
     }
 }
